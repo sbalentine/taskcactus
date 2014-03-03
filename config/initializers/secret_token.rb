c0c454f7892963618667d076aec7f475502bc9fe
Scott Balentine <sbalentine88@gmail.com>
2014-03-03 11:03:27 -0800

    the start of something great

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Taskcactus::Application.config.secret_key_base = 'ec0eb031064e3d0889d3c7367eb1744214382c7b99b478a7566ec8a007fd63bc17f2f8a792f6893f8619c91f2ab921bb1b758cba4641951976ffff314f09b973'
