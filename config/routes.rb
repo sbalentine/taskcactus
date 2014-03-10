26fcfd6e9148fba8930b465d31d7f3bb751cc8bd
scottbalentine <sbalentine88@gmail.com>
2014-03-10 01:55:11 -0700

    changing root routes for testing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Taskcactus::Application.routes.draw do
 
-  resources :home
+  root 'home#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
