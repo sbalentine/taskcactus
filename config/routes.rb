1b068acf3cc5ef84db8e2f1ee790146446ac8474
scottbalentine <sbalentine88@gmail.com>
2014-03-10 00:50:10 -0700

    adding some test files to see if nginx responds

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Taskcactus::Application.routes.draw do
+
+  resources :home
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
